1a22f48c74f57833ffad728791e5aadfd9c58ab2
efserg <mynog@yandex.ru>
2019-04-02 09:27:03 +0300

    Refactoring to ES6 features

--- sum.js
+++ sum.js
@@ -1,17 +1,17 @@
-function sum(x) {
-	var acc = 0;
+const sum = (x) => {
 
-	function inner_sum(x) {
-		if (x) {
-			acc += x;
-			return inner_sum;
-		} else {
-			return acc;
-		}
-	}
+    let acc = 0;
 
-	return inner_sum(x);
+    const innerSum = (x) => {
+        if (x && x !== 0) {
+            acc += x;
+            return innerSum;
+        }
+        return acc;
+    };
+
+    return innerSum(x);
 };
 
 console.log(15 === sum(1)(2)(3)(4)(5)()); // test
-console.log(5 === sum(10)(8)(6)(4)(2)(-1)(-3)(-5)(-7)(-9)()); // test
+console.log(5 === sum(10)(8)(6)(4)(2)(0)(-1)(-3)(-5)(-7)(-9)()); // test
